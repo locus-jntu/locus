4b586320561b0424acccfe74b2b1b41da24e1364
saimaheshtaduri <saimaheshtaduri6@gmail.com>
2022-10-26 23:14:27 +0530

    add ESLint + tailwind + MUI

--- locus/pages/_app.tsx
+++ locus/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from '../utility/createEmotionCache'
+import lightTheme from '../styles/theme/lightTheme'
+import '../styles/globals.css'
+
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = (props: any) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
+
+export default MyApp
