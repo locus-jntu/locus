dce4ebf757d23f3631bc489af1d1117a0f199cba
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-10-28 22:02:09 +0530

    Init backend

---
+++ backend/locus/src/main/java/com/example/locus/LocusApplication.java
@@ -0,0 +1,13 @@
+package com.example.locus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocusApplication.class, args);
+	}
+
+}
