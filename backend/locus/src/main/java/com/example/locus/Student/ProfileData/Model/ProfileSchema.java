c672771a568c9d84c1e628d09d1450cf68fb4996
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-12-22 16:15:11 +0530

    update-profile-schema

--- backend/locus/src/main/java/com/example/locus/Student/ProfileData/Model/ProfileSchema.java
+++ backend/locus/src/main/java/com/example/locus/Student/ProfileData/Model/ProfileSchema.java
@@ -2,7 +2,9 @@ package com.example.locus.Student.ProfileData.Model;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 class Field {
@@ -10,9 +12,10 @@ class Field {
     String type;
     String inputType;
     List<String> values;
+    String placeholder;
 }
 
 @Data
 public class ProfileSchema {
-    List<Field> fixed;
+    Map<String,List<Field>> fixed;
 }
